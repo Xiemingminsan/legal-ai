10623df52e2bb52961c72b77b607fe573a746fc6
surafelzewdu2018@gmail.com <surafelzewdu2018@gmail.com>
2025-01-21 02:20:34 +0300

    added requirment add user new fields

--- backend-node/src/models/User.js
+++ backend-node/src/models/User.js
@@ -2,24 +2,39 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-username: {
- type: String,
- required: true,
- unique: true,
-},
-email: {
- type: String,
- required: true,
- unique: true,
-},
-passwordHash: {
- type: String,
- required: true,
-},
-role: {
- type: String,
- default: 'user', // could be 'admin'
-},
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: 'user', // could be 'admin'
+  },
+  ethiopianLawKnowledge: {
+    type: String,
+    enum: ['Beginner', 'Intermediate', 'Expert'],
+    required: true,
+  },
+  legalProcessUnderstanding: {
+    type: String,
+    enum: ['Low', 'Moderate', 'High'],
+    required: true,
+  },
+  legalTerminologyComfort: {
+    type: String,
+    enum: ['Not Comfortable', 'Somewhat Comfortable', 'Very Comfortable'],
+    required: true,
+  },
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
