35bfae8cecc73f90111c2067040a6187e6ff03cb
surafelzewdu2018@gmail.com <surafelzewdu2018@gmail.com>
2025-03-10 21:52:06 +0300

    pwa done

--- front-end-vue/vite.config.js
+++ front-end-vue/vite.config.js
@@ -1,34 +1,47 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import { VitePWA } from 'vite-plugin-pwa'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
     VitePWA({
       registerType: 'autoUpdate', // Automatically update service worker
+      devOptions: {
+        enabled: true,
+      },
       manifest: {
-        name: 'Your App Name',
-        short_name: 'App Short Name',
-        description: 'Description of your app',
+        name: 'Et legal Bot',
+        short_name: 'Et Legal Bot',
+        description: 'Instant Legal Information',
         theme_color: '#42b983',
         start_url: '/',
         display: 'standalone',
         background_color: '#ffffff',
         icons: [
           {
-            src: '/icons/android-chrome-192x192.png',
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/icons/android-chrome-512x512.png',
+            src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
           },
         ],
       },
@@ -39,4 +52,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+  build: {
+    assetsInclude: ['**/*.woff', '**/*.woff2', '**/*.ttf', '**/*.eot', '**/*.svg'], // Include font files in the build
+  },
+});
