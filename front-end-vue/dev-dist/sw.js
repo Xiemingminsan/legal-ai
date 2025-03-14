ac69a8da19f8a4852ce462d45c8c802f42c83ff3
surafelzewdu2018@gmail.com <surafelzewdu2018@gmail.com>
2025-03-14 17:29:33 +0300

    update build process and enhance PWA capabilities

--- front-end-vue/dev-dist/sw.js
+++ front-end-vue/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vtg7m7rl968"
+    "revision": "0.8n93uofpggg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
